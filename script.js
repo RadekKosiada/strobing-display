602b2e036bd418a607dadc6d49ae721cb834545e
RadekKosiada <kosiadar@gmail.com>
2019-02-11 11:15:20 +0100

    Added strobo effect for all lines; Changed the text

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ firstRow.appendChild(firstRowText);
 var secondRow = document.createElement("h3");
 var secondRowText = document.createTextNode("");
 secondRow.className = "second-row";
-secondRow.setAttribute("data-wait", 9*speed);
+secondRow.setAttribute("data-wait", 9*typingSpeed
+);
 secondRow.appendChild(secondRowText);
 
 var thirdRow = document.createElement("h3");
@@ -30,76 +31,67 @@ container.appendChild(thirdRow);
 
 console.log(firstRow.innerText.length);
 
-// firstRow.style.display = "none";
-// secondRow.style.display = "none";
-// line.style.display = "none";
-// lineTwo.style.display = "none";
-// thirdRow.style.display = "none";
-
-
-// var turn = 0;
-// var texts = ["Berlinale, 07.02 - 17.02.2019, >> Preview <<"];
-
-var firstText = "Berlinale";
-var secondText ="07.02 - 17.02.2019";
-var thirdText = ">> Preview <<";
-
-// function typeLetters(nodeElement) {
-//   if(pos < texts[turn].length) {
-//     nodeElement.innerHTML += texts[turn].charAt(pos);
-//     console.log(texts[turn].charAt(pos));
-//     pos++;
-//     setTimeout(typeLetters.bind(null, nodeElement), speed);
-//   }
-// }
-
-// setTimeout(typeLetters.bind(null, firstRow), speed);
+var firstText = "This week in Berlin";
+var secondText ="13.02 - 18.02.2019";
+var thirdText = ">> Aleksa <<";
 
 var pos = 0;
-var speed = 100;
+var typingSpeed = 100;
 function typeLetters(nodeElement, string) {
   if(pos < string.length) {
     nodeElement.innerHTML += string.charAt(pos);
-    console.log(string.charAt(pos));
+    // console.log(string.charAt(pos));
     pos++;
-    setTimeout(typeLetters.bind(null, nodeElement, string), speed);
+    setTimeout(typeLetters.bind(null, nodeElement, string), typingSpeed);
   }
 }
 
-setTimeout(typeLetters.bind(null, firstRow, firstText), speed);
+setTimeout(typeLetters.bind(null, firstRow, firstText), typingSpeed);
 
-// setTimeout(typeLetters.bind(null, secondRow, "whatever"), speed);
+// var stroboSpeed = 50;
+function makeInvisible(nodeElement) {
+  console.log("makeInvisible fired")
+  nodeElement.className = "strobo-effect";
+}
+
+setTimeout(makeInvisible.bind(null, firstRow), typingSpeed*(firstText.length+1));
 
 var pos2 = 0;
+var stroboDelay = 15;
+
 function typeLetters2(nodeElement, string) {
-  // console.log("fired")
+  console.log("fired")
   if(pos2 < string.length) {
     nodeElement.innerHTML += string.charAt(pos2);
-    console.log(string.charAt(pos2));
+    // console.log(string.charAt(pos2));
     pos2++;
-    setTimeout(typeLetters2.bind(null, nodeElement, string), speed);
+    setTimeout(typeLetters2.bind(null, nodeElement, string), typingSpeed);
   }
 }
 
-setTimeout(typeLetters2.bind(null, secondRow, secondText), speed*(firstText.length+1));
+setTimeout(typeLetters2.bind(null, secondRow, secondText), typingSpeed*(firstText.length+stroboDelay));
+
+setTimeout(makeInvisible.bind(null, secondRow), typingSpeed *(secondText.length + firstText.length+stroboDelay));
 
 
 var pos3 = 0;
 function typeLetters3(nodeElement, string) {
-  console.log("fired third function")
+  // console.log("fired third function")
   if(pos3 < string.length) {
     nodeElement.innerHTML += string.charAt(pos3);
     console.log(string.charAt(pos3));
     pos3++;
-    setTimeout(typeLetters3.bind(null, nodeElement, string), speed);
+    setTimeout(typeLetters3.bind(null, nodeElement, string), typingSpeed);
   }
 }
 
-setTimeout(typeLetters3.bind(null, thirdRow, thirdText), speed * (secondText.length + firstText.length+1));
+setTimeout(typeLetters3.bind(null, thirdRow, thirdText), typingSpeed *(secondText.length + firstText.length+stroboDelay*2));
+setTimeout(makeInvisible.bind(null, thirdRow), typingSpeed*(secondText.length + firstText.length+stroboDelay*3));
 
 function changeBackground() {
   // document.body.style.backgroundColor = "yellow";
   document.body.className ="on";
+  // container.className = "container strobo-container";
 }
 
-setTimeout(changeBackground, speed * (secondText.length + firstText.length + thirdText.length +1));
+setTimeout(changeBackground, typingSpeed *(secondText.length + firstText.length+stroboDelay*4));
